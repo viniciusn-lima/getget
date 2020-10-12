0765ec91d14512c6d24b9eed848c9ca085174e34
viniciusn-lima <viniciusn.lima@outlook.com>
2020-10-12 17:50:22 -0300

    Add new features

--- app.py
+++ app.py
@@ -24,6 +24,12 @@ def inserir_many_anuncio():
     content = request.json
     return insert_many_anuncio(content)
 
-if __name__ == '__main__':
-    from waitress import serve
-    app.run(host='0.0.0.0',port='8080',debug=False)
+@app.route("/")
+def hello():
+    return "Hello, World!"
+#if __name__ == '__main__':
+#    from waitress import serve
+#    #app.run(host='0.0.0.0',port='8080',debug=False)
+#    app.run(port='8080',debug=False)
+
+
